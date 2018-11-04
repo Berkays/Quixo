abc41287ede8eda95f0ef9f29bfd8f0e6379cf31
Berkay GURSOY <berkaygursoy@gmail.com>
2018-11-04 19:51:32 +0300

    add game methods

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import shuffle
 
 class Board(object):
     BOARD_SIZE = 4 # 0 indexed
@@ -14,33 +15,74 @@ class Board(object):
     X = 1
     O = 2
 
-    def __init__(self):
+    def __init__(self,board=None):
         self.turn = self.X
-        self.board = np.zeros((5,5),dtype=int)
+        if(board == None):
+            self.board = np.zeros((5,5),dtype=int)
+        else:
+            self.board = np.copy(board.board)
+            self.turn = board.turn
 
     def play(self,piece,move):
-        legalMove = self.checkMove(piece,move)
-        if(not legalMove):
-            raise Exception('Illegal Move')
+        # legalMove = self.checkMove(piece,move)
+        # if(not legalMove):
+        #     raise Exception('Illegal Move')
 
         self.board[piece] = self.turn
-        #self.shiftBoard(piece,move)
         self.shift(piece,move)
         self.changeTurn()
-
 
     def checkMove(self,piece,move):
         #Only blank and owned pieces
         if(not (self.board[piece] == self.BLANK or self.board[piece] == self.turn)):
             return False
 
-        possibleMoves = self.getPossibleMoves(piece)
+        possibleMoves = self.getPossiblePieceMoves(piece)
         if(move in possibleMoves):
             return True
         else:
             return False
 
-    def getPossibleMoves(self,piece):
+    def shift(self,piece,move):
+        row,col = piece
+
+        if(move == self.SLIDE_RIGHT):
+            self.board[row,0:col + 1] = np.roll(self.board[row,0:col + 1],1)
+        elif(move == self.SLIDE_LEFT):
+            self.board[row,col:self.BOARD_SIZE + 1] = np.roll(self.board[row,col:self.BOARD_SIZE + 1],-1)
+        elif(move == self.SLIDE_UP):
+            self.board[row:self.BOARD_SIZE + 1,col] = np.roll(self.board[row:self.BOARD_SIZE + 1,col],-1)
+        else:
+            self.board[0:row + 1,col] = np.roll(self.board[0:row + 1,col],1)
+
+        return
+
+    def changeTurn(self):
+        if(self.turn == self.X):
+            self.turn = self.O
+        else:
+            self.turn = self.X
+
+    def isGameEnd(self):
+        firstDiagonal = np.diagonal(self.board)
+        secondDiagonal = np.diagonal(np.rot90(self.board))
+
+        if(np.all(firstDiagonal == self.X) or np.all(firstDiagonal == self.O)):
+            return self.board[0,0]
+        if(np.all(secondDiagonal == self.X) or np.all(secondDiagonal == self.O)):
+            return self.board[0,self.BOARD_SIZE]
+        
+        for i in range(0,self.BOARD_SIZE + 1):
+            row = self.board[i,:]
+            col = self.board[:,i]
+            if(np.all(row == self.X) or np.all(row == self.O)):
+                return self.board[i,0]
+            if(np.all(col == self.X) or np.all(col == self.O)):
+                return self.board[0,i]
+        
+        return None
+      
+    def getPossiblePieceMoves(self,piece):
         row = piece[0]
         col = piece[1]
         possibleMoves = []
@@ -56,60 +98,30 @@ class Board(object):
 
         return possibleMoves
 
-    def shiftBoard(self,piece,move):
-        row = piece[0]
-        col = piece[1]
-        if(move == self.SLIDE_LEFT):
-            self.board[row,:] = np.roll(self.board[row,:],-1)
-        elif(move == self.SLIDE_RIGHT):
-            self.board[row, :] = np.roll(self.board[row, :], 1)
-        elif(move == self.SLIDE_UP):
-            self.board[:, col] = np.roll(self.board[:, col], -1)
-        elif(move == self.SLIDE_DOWN):
-            self.board[:, col] = np.roll(self.board[:, col], 1)
-
-    def shift(self,piece,move):
-        row,col = piece
-
-        if(move == self.SLIDE_RIGHT):
-            self.board[row,0:col + 1] = np.roll(self.board[row,0:col + 1],1)
-        elif(move == self.SLIDE_LEFT):
-            self.board[row,col:self.BOARD_SIZE + 1] = np.roll(self.board[row,col:self.BOARD_SIZE + 1],-1)
-        elif(move == self.SLIDE_UP):
-            self.board[row:self.BOARD_SIZE + 1,col] = np.roll(self.board[row:self.BOARD_SIZE + 1,col],1,axis=1)
-        else:
-            self.board[0:row + 1,col] = np.roll(self.board[0:row + 1,col],-1,axis=1)
-
-        
-        return
-
-    def changeTurn(self):
+    def getPossibleMoves(self):
         if(self.turn == self.X):
-            self.turn = self.O
+            arg = self.O 
         else:
-            self.turn = self.X
+            arg = self.X
 
-    def getAllPossibleMoves(self):
-        edgeTuples = []
-        x = self.board.copy()
-        x[:,0] = 1
-        x[:,self.BOARD_SIZE] = 1
-        x[0,:] = 1
-        x[self.BOARD_SIZE,:] = 1
-        
-        for i in range(0, self.BOARD_SIZE + 1):
-            for k in range(0, self.BOARD_SIZE + 1):
-                if(x[(i,k)] == 1 and (i,k) not in edgeTuples and self.board[(i,k)] == self.BLANK or self.board[(i,k)] == self.turn):
-                    edgeTuples.append((i,k))
+        edgeMatrix = np.full((5,5),arg)
+        edgeMatrix[0,:] = self.board[0,:]
+        edgeMatrix[4,:] = self.board[4,:]
+        edgeMatrix[:,0] = self.board[:,0]
+        edgeMatrix[:,4] = self.board[:,4]
 
+        edgeIndices = np.where(edgeMatrix != arg)
+        edgeIndices = np.column_stack((edgeIndices[0],edgeIndices[1]))
         allMoves = []
-        for piece in edgeTuples:
-            moves = self.getPossibleMoves(piece)
+        for piece in edgeIndices:
+            piece = (piece[0],piece[1])
+            moves = self.getPossiblePieceMoves(piece)
             for move in moves:
                 allMoves.append((piece,move))
 
-        return allMoves
-        
+        shuffle(allMoves)
+
+        return allMoves       
 
     def printTurn(self):
         if(self.turn == self.X):
